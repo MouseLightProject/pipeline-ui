4f47dbafbf0744d4006d91669732681917fe8bfd
Patrick Edson <pedson@users.noreply.github.com>
2017-11-14 10:52:08 -0500

    Remove unused.

--- client/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export class Loading extends React.Component<any, any> {
-    render() {
-        return (
-            <h3 className="text-center">Loading...</h3>
-        );
-    }
-}
